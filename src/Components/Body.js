fad7f01edded166d47a5d1a565d6637fbf4f1c9e
Farooque <farooque.reactjs@gmail.com>
2024-02-03 02:00:09 +0530

    forth commit

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,55 +1,60 @@
-import React, { useState } from "react";
-import { restaurantList } from "./Config.js";
-import RestrauntCard from "./RestronantCard";
-
-function filterData(searchText, restraunts) {
-  const filterData = restraunts.filter((restraunts) =>
-    restraunt.data.name.includes(searchText)
-  );
-  return filterData();
-}
+import { restaurantList } from "../contants";
+import RestaurantCard from "./RestaurantCard";
+import { useState, useEffect, useContext } from "react";
+import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
+import { filterData } from "../utils/helper";
+import useOnline from "../utils/useOnline";
+import UserContext from "../utils/UserContext";
 
 const Body = () => {
-  const [searchText, setsearchText] = useState("");
-  const [restaurants, setrestaurants] = useState(restaurantList);
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const { user, setUser } = useContext(UserContext);
 
+  useEffect(() => {
+    getRestaurants();
+  }, []);
 
-useEffect (()=>{
-  //API call
-  fetch("")
-},[searchText])
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+  if (!allRestaurants) return null;
 
-async function getRestraunt() {
-  const data = await fetch("")
-}
-
-  return (
+  return allRestaurants?.length === 0 ? (
+    <Shimmer />
+  ) : (
     <>
-      <div className="search-container">
+      <div className="search-container p-5 bg-pink-50 my-5">
         <input
+          data-testid="search-input"
           type="text"
-          className="search-input"
+          className="focus:bg-green-200 p-2 m-2"
           placeholder="Search"
           value={searchText}
           onChange={(e) => {
-            setsearchText(e.target.value)
+            setSearchText(e.target.value);
           }}
         />
         <button
-          className="search-btn"
+          data-testid="search-btn"
+          className="p-2 m-2 bg-purple-900 hover:bg-gray-500 text-white rounded-md"
           onClick={() => {
-            const data = filterData(searchText, restrauntlist);
-            setsearchRest(data);
+            //need to filter the data
+            const data = filterData(searchText, allRestaurants);
+            // update the state - restaurants
+            setFilteredRestaurants(data);
           }}
         >
           Search
         </button>
-      </div>
-      <div className="search-container">
         <input
-          type="text"
-          className="search-input"
-          placeholder="Search"
           value={user.name}
           onChange={(e) =>
             setUser({
@@ -57,15 +62,32 @@ async function getRestraunt() {
               name: e.target.value,
             })
           }
-        />
+        ></input>
+        <input
+          value={user.email}
+          onChange={(e) =>
+            setUser({
+              ...user,
+              email: e.target.value,
+            })
+          }
+        ></input>
       </div>
-      <div className="restarurant-List">
-        {restrautList.mp((restrurant) => {
+      <div className="flex flex-wrap " data-testid="res-list">
+        {/* You have to write logic for NO restraunt fount here */}
+        {filteredRestaurants.map((restaurant) => {
           return (
-            <RestrauntCard {...restrurant.data} key={restrurant.data.id} />
+            <Link
+              to={"/restaurant/" + restaurant.data.id}
+              key={restaurant.data.id}
+            >
+              <RestaurantCard {...restaurant.data} />
+            </Link>
           );
         })}
       </div>
     </>
   );
 };
+
+export default Body;
--- src/Components/RestronantCard.js
+++ src/Components/RestronantCard.js
@@ -1,17 +1,23 @@
-import { IMG_CDN_URL, restaurantList } from "./Config";
+import { IMG_CDN_URL } from "../contants";
+import { useContext } from "react";
+import UserContext from "../utils/UserContext";
 
 const RestrauntCard = ({
   name,
   cuisines,
-  cloudinaryImageID,
+  cloudinaryImageId,
   lastMileTravelString,
 }) => {
+  const { user } = useContext(UserContext);
   return (
-    <div className="card">
-      <img src={IMG_CDN_URL + cloudinaryImageID} />
-      <h2>{name}</h2>
+    <div className="w-56 p-2 m-2 shadow-lg bg-pink-50">
+      <img src={IMG_CDN_URL + cloudinaryImageId} />
+      <h2 className="font-bold text-xl">{name}</h2>
       <h3>{cuisines.join(", ")}</h3>
       <h4>{lastMileTravelString}</h4>
+      <h5 className="font-bold">
+        {user.name} - {user.email}
+      </h5>
     </div>
   );
 };
